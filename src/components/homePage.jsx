6f0e4bcdf8c5388e765d6e31e183576dd27803da
Davidcialone <cialonedavid@gmail.com>
2024-11-29 14:00:09 +0100

    adjust title responsive

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,13 +3,15 @@ import React from "react";
 export function HomePage() {
   return (
     <div>
-      <h1 className="bg-slate-500 m-4 shadow text-white text-3xl font-bold">
-        <span className="text-6xl font-extrabold text-blue-400">A</span>telier{" "}
-        <span className="text-6xl font-extrabold text-blue-400">T</span>echnique{" "}
-        <span className="text-6xl font-extrabold text-blue-400">O</span>xycoupage{" "}
-        <span className="text-6xl font-extrabold text-blue-400">L</span>aser{" "}
-        <span className="text-6xl font-extrabold text-blue-400">L</span>yonnais
-      </h1>
+    <h1 className="text-white text-xl sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold">
+      <span className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold text-blue-400">A</span>telier{" "}
+      <span className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold text-blue-400">T</span>echnique{" "}
+      <span className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold text-blue-400">O</span>xycoupage{" "}
+      <span className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold text-blue-400">L</span>aser{" "}
+      <span className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-extrabold text-blue-400">L</span>yonnais
+    </h1>
+
+
 
       <p className="text-xl font-bold m-4">
             
@@ -23,6 +25,7 @@ export function HomePage() {
         muted
         loop 
         width="100%" 
+        className="max-w-[1024px]  mx-auto" 
         height="auto">
         Votre navigateur ne supporte pas la lecture de vidéo.
       </video>
