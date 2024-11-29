e0fe21dedc5ef4a7ee2af69d7ef4f4588c390d39
Davidcialone <cialonedavid@gmail.com>
2024-11-29 12:01:08 +0100

    adjust title

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 export function HomePage() {
   return (
-    <div >
-      
-      <h1>Bienvenue chez Atoll</h1>
-      <p>Spécialistes de la découpe laser, pliage et oxycoupage à Lyon</p>
-      <p>Qualité, précision et rapidité au service de vos projets métallurgique</p>
+    <div>
+      <h1 className="bg-slate-500 m-4 shadow text-white text-3xl font-bold">
+        <span className="text-6xl font-extrabold text-blue-400">A</span>telier{" "}
+        <span className="text-6xl font-extrabold text-blue-400">T</span>echnique{" "}
+        <span className="text-6xl font-extrabold text-blue-400">O</span>xycoupage{" "}
+        <span className="text-6xl font-extrabold text-blue-400">L</span>aser{" "}
+        <span className="text-6xl font-extrabold text-blue-400">L</span>yonnais
+      </h1>
+
+      <p className="text-xl font-bold m-4">
+            
+        Spécialistes de la découpe laser, pliage et oxycoupage à Lyon
+        qualité, précision et rapidité au service de vos projets métallurgiques.
+      </p>
       <video 
         src="decoupe-vid2.mp4" 
         controls 
@@ -17,11 +26,6 @@ export function HomePage() {
         height="auto">
         Votre navigateur ne supporte pas la lecture de vidéo.
       </video>
-
-     
-
-      </div>
+    </div>
   );
 }
-
-
